3a5d4240dcdd99c449cab7a6f5882157bf4c3f34
DireAussie <dfidge@internode.on.net>
2019-12-18 22:12:49 +1030

    clean up versioning and nuget push

--- Build-NuGetRelease.ps1
+++ Build-NuGetRelease.ps1
@@ -3,9 +3,17 @@ param
     [Parameter(Mandatory=$true)]
     [string]$Version,
     [Parameter(Mandatory=$true)]
-    [string]$NuGetApiKey
+    [string]$NuGetApiKey,
+    [switch]$UseWindowsDX
 )
+
+$WindowsDX = ""
+
+if ($UseWindowsDX -eq $true)
+{
+    $WindowsDX = ".WindowsDX"
+}
 
 msbuild .\InputHandlers.sln /p:Configuration=Release
 nuget pack .\InputHandlers\
-nuget push ".\InputHandlers.MonoGame.$($version).nupkg" -Source https://nuget.org -ApiKey $nuGetApiKey
+nuget push ".\InputHandlers.MonoGame$($WindowsDX).$($Version).nupkg" -Source https://nuget.org -ApiKey $NuGetApiKey
